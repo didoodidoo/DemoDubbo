0f1123cd8598e0062fe9672a40744ac9a4a78693
konwnt <drdiaorui@163.com>
2019-08-14 15:24:50 +0800

    IDEA创建的DUBBO项目

---
+++ dubboproject/src/main/java/cn/code/dubboproject/DubboProjectApplication.java
@@ -0,0 +1,14 @@
+package cn.code.dubboproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProjectApplication.class, args);
+    }
+
+
+}
--- dubboproject/src/test/java/cn/code/dubboproject/DubboprojectApplicationTests.java
+++ dubboproject/src/test/java/cn/code/dubboproject/DubboprojectApplicationTests.java
@@ -1,4 +1,4 @@
-package cn.code;
+package cn.code.dubboproject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoDubboApplicationTests {
+public class DubboprojectApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
