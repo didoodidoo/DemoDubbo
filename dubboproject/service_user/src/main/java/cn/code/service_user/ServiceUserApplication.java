b541782e2878d56832fba157467f88d22a5534eb
konwnt <drdiaorui@163.com>
2019-08-15 09:48:55 +0800

    添加service_user模块

---
+++ dubboproject/service_user/src/main/java/cn/code/service_user/ServiceUserApplication.java
@@ -0,0 +1,13 @@
+package cn.code.service_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUserApplication.class, args);
+    }
+
+}
