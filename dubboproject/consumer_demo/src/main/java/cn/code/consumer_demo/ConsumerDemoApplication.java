7f4181c59c4b61feb293b4cb8fc89b25845a78f2
konwnt <drdiaorui@163.com>
2019-08-16 16:39:24 +0800

    添加一个消费者

---
+++ dubboproject/consumer_demo/src/main/java/cn/code/consumer_demo/ConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.code.consumer_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerDemoApplication.class, args);
+    }
+
+}
