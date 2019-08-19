a340b9b1b97eb21c953ff933bd17521a8387dcc8
konwnt <drdiaorui@163.com>
2019-08-19 16:17:59 +0800

    Markdown上传图片很不方便

--- Blog/src/main/java/cn/code/blog/controller/IndexController.java
+++ Blog/src/main/java/cn/code/blog/controller/IndexController.java
@@ -1,11 +1,18 @@
 package cn.code.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+	
+	@RequestMapping(value = "/{path}")
+	public String path(@PathVariable String path) {
+		return path;
+	}
+	
 	@RequestMapping(value = "/index")
 	public String index() {
 		return "index";
