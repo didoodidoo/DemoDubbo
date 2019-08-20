847380efddb5907b8d06f8e4c5f73335fd135888
konwnt <drdiaorui@163.com>
2019-08-20 19:03:56 +0800

    添加了一个消费者

--- dubboproject/consumer_demo/src/main/java/cn/code/consumer_demo/ConsumerDemoApplication.java
+++ dubboproject/consumer_demo/src/main/java/cn/code/consumer_demo/ConsumerDemoApplication.java
@@ -1,8 +1,10 @@
 package cn.code.consumer_demo;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubboConfiguration
 @SpringBootApplication
 public class ConsumerDemoApplication {
 
--- dubboproject/consumer_demo/src/main/java/cn/code/consumer_demo/controller/IndexController.java
+++ dubboproject/consumer_demo/src/main/java/cn/code/consumer_demo/controller/IndexController.java
@@ -1,13 +1,24 @@
 package cn.code.consumer_demo.controller;
 
+import cn.code.service_user.service.IUserService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Reference(version = "1.0.0")
+    IUserService service;
 
     @RequestMapping(value = "/index")
+    @ResponseBody
     public String index() {
+        System.out.println(service.getUserNum());
         return "DUBBO HELLO";
     }
 
